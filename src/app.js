8276e6a09b497dce0de51b6204fd1059d04dc68f
Javier Obando <bonilla.javier@protonmail.com>
2025-08-21 04:40:57 +0000

    Generador de Excusas - Version 1.0

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuseElement = document.querySelector('#excuse')
+  function GetRandomData(array) {
+    const RandomData = Math.floor(Math.random()* array.length);
+    return array[RandomData]
+  }
+  
+  const who = ['El perro', 'Mi abuela', 'El cartero', 'Un pajaro'];
+  const RandomWho = GetRandomData(who)
+  const action = ['comio',  'orino', 'trituro', 'quebro'];
+  const RandomAction = GetRandomData(action)
+  const what = ['mi tarea', 'mi celular', 'el carro'];
+  const RandomWhat = GetRandomData(what)
+  const when = ['antes de la clase', 'cuando estaba durmiendo', 'mientras me ejercitaba', 'durante mi almuerzo', 'cuando estaba rezando'];
+  const RandomWhen = GetRandomData(when)
+
+  const excuse = RandomWho + " " + RandomAction + " " + RandomWhat + " " + RandomWhen
+  excuseElement.innerText = excuse 
 };
+
